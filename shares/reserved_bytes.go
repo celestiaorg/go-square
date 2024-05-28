4063a54ec963df4d2dea7b818031421e35d06cb5
Rootul P <rootulp@gmail.com>
2024-05-28 09:42:50 -0400

    refactor: rename to ShareReservedBytes (#61)

--- shares/reserved_bytes.go
+++ shares/reserved_bytes.go
@@ -6,22 +6,22 @@ import (
 )
 
 // NewReservedBytes returns a byte slice of length
-// CompactShareReservedBytes that contains the byteIndex of the first
+// ShareReservedBytes that contains the byteIndex of the first
 // unit that starts in a compact share.
 func NewReservedBytes(byteIndex uint32) ([]byte, error) {
 	if byteIndex >= ShareSize {
 		return []byte{}, fmt.Errorf("byte index %d must be less than share size %d", byteIndex, ShareSize)
 	}
-	reservedBytes := make([]byte, CompactShareReservedBytes)
+	reservedBytes := make([]byte, ShareReservedBytes)
 	binary.BigEndian.PutUint32(reservedBytes, byteIndex)
 	return reservedBytes, nil
 }
 
 // ParseReservedBytes parses a byte slice of length
-// CompactShareReservedBytes into a byteIndex.
+// ShareReservedBytes into a byteIndex.
 func ParseReservedBytes(reservedBytes []byte) (uint32, error) {
-	if len(reservedBytes) != CompactShareReservedBytes {
-		return 0, fmt.Errorf("reserved bytes must be of length %d", CompactShareReservedBytes)
+	if len(reservedBytes) != ShareReservedBytes {
+		return 0, fmt.Errorf("reserved bytes must be of length %d", ShareReservedBytes)
 	}
 	byteIndex := binary.BigEndian.Uint32(reservedBytes)
 	if ShareSize <= byteIndex {
--- shares/shares.go
+++ shares/shares.go
@@ -186,7 +186,7 @@ func (s *Share) rawDataStartIndex() int {
 		index += SequenceLenBytes
 	}
 	if isCompact {
-		index += CompactShareReservedBytes
+		index += ShareReservedBytes
 	}
 	return index
 }
@@ -227,7 +227,7 @@ func (s *Share) rawDataStartIndexUsingReserved() (int, error) {
 	}
 
 	if isCompact {
-		reservedBytes, err := ParseReservedBytes(s.data[index : index+CompactShareReservedBytes])
+		reservedBytes, err := ParseReservedBytes(s.data[index : index+ShareReservedBytes])
 		if err != nil {
 			return 0, err
 		}
