539b7668b6e20506f9b80af80e662f9d636442ad
Rootul Patel <rootulp@gmail.com>
2025-08-21 15:50:45 +0200

    test: sequence len

---
+++ share/mocha_shares_test.go
@@ -0,0 +1,31 @@
+package share
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// Reproduces https://github.com/celestiaorg/celestia-node/issues/4490
+func TestMochaShares(t *testing.T) {
+
+	jsonData, err := os.ReadFile("./testdata/mocha-shares.json")
+	require.NoError(t, err)
+
+	var shares []Share
+	err = json.Unmarshal(jsonData, &shares)
+	require.NoError(t, err)
+
+	require.Equal(t, 3423, len(shares))
+	require.True(t, shares[0].IsSequenceStart())
+	require.False(t, shares[1].IsSequenceStart())
+	require.False(t, shares[3422].IsSequenceStart())
+
+	wantNamespace, err := NewV0Namespace([]byte{0x72, 0x65, 0x6e, 0x65, 0x72, 0x65, 0x6e, 0x65})
+	require.NoError(t, err)
+	for _, share := range shares {
+		require.Equal(t, wantNamespace, share.Namespace())
+	}
+}
