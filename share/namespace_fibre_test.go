27f91a5efda68eec55e9df4e1d4f6b7553a211b5
Rootul Patel <rootulp@gmail.com>
2025-10-30 16:44:50 -0400

    refactor: test

--- share/namespace_fibre_test.go
+++ share/namespace_fibre_test.go
@@ -8,38 +8,15 @@ import (
 )
 
 func TestPayForFibreNamespace(t *testing.T) {
-	// Verify PayForFibreNamespace has the correct value: 0x0000000000000000000000000000000000000000000000000000000005
-	expectedBytes := []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05}
-	actualBytes := PayForFibreNamespace.Bytes()
-	assert.Equal(t, expectedBytes, actualBytes, "PayForFibreNamespace should be 0x0000000000000000000000000000000000000000000000000000000005")
-}
-
-func TestIsPayForFibre(t *testing.T) {
-	type testCase struct {
-		ns   Namespace
-		want bool
-	}
-	testCases := []testCase{
-		{
-			ns:   PayForFibreNamespace,
-			want: true,
-		},
-		{
-			ns:   PayForBlobNamespace,
-			want: false,
-		},
-		{
-			ns:   TxNamespace,
-			want: false,
-		},
-		{
-			ns:   MustNewV0Namespace(bytes.Repeat([]byte{1}, NamespaceVersionZeroIDSize)),
-			want: false,
-		},
-	}
-
-	for _, tc := range testCases {
-		got := tc.ns.IsPayForFibre()
-		assert.Equal(t, tc.want, got, "IsPayForFibre() for namespace %x", tc.ns.Bytes())
-	}
+	t.Run("Bytes", func(t *testing.T) {
+		want := []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05}
+		got := PayForFibreNamespace.Bytes()
+		assert.Equal(t, want, got)
+	})
+	t.Run("IsPayForFibre", func(t *testing.T) {
+		assert.True(t, PayForFibreNamespace.IsPayForFibre())
+		assert.False(t, PayForBlobNamespace.IsPayForFibre())
+		assert.False(t, TxNamespace.IsPayForFibre())
+		assert.False(t, MustNewV0Namespace(bytes.Repeat([]byte{1}, NamespaceVersionZeroIDSize)).IsPayForFibre())
+	})
 }
